387ab9d5f06246f8c45ec78ff169266df10ea2c4
marmoi <imarmo5@alumni.uv.es>
2020-06-08 10:40:39 +0200

    update requirements

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
-requirements = ["tensorflow>=1.15.0"]
+requirements = ["tensorflow>=1"]
 
 setup(
     name="tracepooling-layer",
